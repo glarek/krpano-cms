572e6a021ac1d6ff636df9778ce1247a3322f8ff
glare <adrian.larek@gmail.com>
2025-12-22 12:37:19 +0100

    Major API rewrite

--- start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Krpano CMS Server...
-echo Limits: Upload=256MB, Post=256MB, Memory=512M
-echo Extensions: Zip
-echo Protection: Active (using router.php)
-echo Access at http://localhost:8000/admin/
-php -d upload_max_filesize=256M -d post_max_size=256M -d memory_limit=512M -d extension=zip -S localhost:8000 router.php
-pause
